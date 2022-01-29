6e9148333448b204d9de709d9c8a4b8657f0343f
shitein <98593768+shitein@users.noreply.github.com>
2022-01-29 09:12:11 +0530

    Create RoleModel.php

---
+++ src/models/RoleModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Shitein\Menus\src\models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class RoleModel extends Authenticatable
+{
+	 //Use Updater;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['company_id', 'role', 'redirect_url', 'cretaed_by', 'updated_by', 'created_at', 'updated_at'];
+    public $table       = 'roles';
+}
