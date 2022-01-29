4aba901ce59e0758bb5b3c0ca3d192b37cdb2c3e
shitein <98593768+shitein@users.noreply.github.com>
2022-01-29 08:52:53 +0530

    Create MenuServiceProvider.php

---
+++ src/MenuServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Package\Menus\src;
+
+use Illuminate\Support\ServiceProvider;
+
+class MenusServiceProvider extends ServiceProvider {
+    public function register() {
+
+    }
+
+    public function boot() {
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'Menus');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
+        $this->publishes([
+            __DIR__.'/public' => public_path(''),
+        ], 'public');
+    }
+}
