1be23ed59a0d7a516d28cc420f9aa3632efd81e1
shitein <98593768+shitein@users.noreply.github.com>
2022-01-29 09:28:02 +0530

    Update Controller.php

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Shitein\Menus\src\controllers;
+namespace Shitein\Menus\controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
