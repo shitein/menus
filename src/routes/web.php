d17df36586d91e56a7235ad327040677e4a63d16
shitein <98593768+shitein@users.noreply.github.com>
2022-01-29 08:53:44 +0530

    Create web.php

---
+++ src/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(['namespace' => 'App\Package\Menus\src\controllers', /*'prefix' => 'auth',*/ 'middleware' => ['web']], function() {
+    Route::resource('/menus', 'MenusController');
+    Route::get('addMenuMappingItem', 'MenusController@addMenuMappingItem');
+    // ** Edit Menu Route **//
+    Route::get('editMenuMapping', 'MenusController@editMenuMapping');
+    Route::post('/menus/update', 'MenusController@Update');
+    Route::get('deleteMenuMappingAndRoles', 'MenusController@deleteMenuMappingAndRoles');
+
+    Route::get('userAccessRole', 'MenusController@userAccessRole');
+    Route::post('save-user-access-role', 'MenusController@save_user_role_access');
+    Route::get('usermenuAccessTypes', 'MenusController@usermenuAccessTypes');
+    // ** menus dragdrop **//
+    Route::post('updateMenuList', 'MenusController@updateMenuList');
+    // **   menus report  **/
+    Route::get('menu_master_report', 'MenusReportController@index');
+    /** * Export Excel */
+    Route::get('export_to_excel', 'ExportController@export');
+});
