a10178f0a6d3e1838f87f0d0711f15ba1366d807
shitein <98593768+shitein@users.noreply.github.com>
2022-01-29 08:59:14 +0530

    Create Menus.php

---
+++ src/models/Menus.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Package\Menus\src\models;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Exception;
+
+class Menus extends Authenticatable
+{
+	 //Use Updater;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id', 'short_code ', 'description', 'url','class', 'font_icon_class',
+                         'language_key ', 'styles'];
+
+    public $table       = 'menus';
+    public function menumappings()
+    {
+        return $this->hasMany(MenusMapping::class,'menu_id', 'id');
+    }
+}
