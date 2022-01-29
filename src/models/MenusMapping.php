64dfe609d71966e85df4006850688b8f0dd509de
shitein <98593768+shitein@users.noreply.github.com>
2022-01-29 08:59:54 +0530

    Create MenusMapping.php

---
+++ src/models/MenusMapping.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Package\Menus\src\models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class MenusMapping extends Authenticatable
+{
+	 //Use Updater;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['menu_master_id', 'menu_id', 'parent_id', 'reference_id', 'short_code', 'sequence', 'url', 'class', 'font_icon_class', 'is_deleted'];
+    public $table       = 'menu_mapping';
+
+    public function menus()
+    {
+        return $this->belongsTo(Menus::class);
+    }
+}
